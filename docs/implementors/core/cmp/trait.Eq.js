96853cebc9e3e1e7c638ffdb172c3f20389219eb
Jesse C. Grillo <jesse.grillo@gmail.com>
2016-12-01 03:06:43 +0000

    move rustdocs to /docs

---
+++ docs/implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["forecast"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='forecast/enum.Icon.html' title='forecast::Icon'>Icon</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='forecast/enum.PrecipType.html' title='forecast::PrecipType'>PrecipType</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='forecast/enum.ExcludeBlock.html' title='forecast::ExcludeBlock'>ExcludeBlock</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='forecast/enum.ExtendBy.html' title='forecast::ExtendBy'>ExtendBy</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='forecast/enum.Lang.html' title='forecast::Lang'>Lang</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='forecast/enum.Units.html' title='forecast::Units'>Units</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='forecast/struct.Alert.html' title='forecast::Alert'>Alert</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
