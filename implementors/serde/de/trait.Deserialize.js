d65b76e60a70745d604a52e1fa0ffb55b29ae9d8
Jesse C. Grillo <jesse.grillo@gmail.com>
2018-06-02 21:13:07 +0000

    v1.1.0 docs update

---
+++ implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["forecast"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.27/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"forecast/enum.Lang.html\" title=\"enum forecast::Lang\">Lang</a>",synthetic:false,types:["forecast::Lang"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["forecast"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.27/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"forecast/enum.Lang.html\" title=\"enum forecast::Lang\">Lang</a>",synthetic:false,types:["forecast::Lang"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
